5f4e8f60c369f894e8aa10150fed687a7dc7666b
Amjad Yahia Roben Hassan <amjedyehia1998@gmail.com>
2022-01-31 21:11:09 +0200

    add cv file

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -7,38 +7,43 @@ import {
 } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
-import { profilePic, container } from './profile-pic.module.css';
+import { profilePic, container, downloadBtn } from './profile-pic.module.css';
 import NavDropdown from '../NavDropDown';
-const Hero = (props) => (
-  <>
-    <Section row nopadding>
-      {/* <NavDropdown /> */}
-      <LeftSection>
-        <div className={container}>
-          <Image
-            className={profilePic}
-            priority={true}
-            placeholder='blue'
-            src='/images/me.jpeg'
-            quality={100}
-            width={250}
-            height={250}
-            alt='Picture of the Amjad Yahia'
-          />
-        </div>
+const Hero = (props) => {
+  return (
+    <>
+      <Section row nopadding>
+        {/* <NavDropdown /> */}
+        <LeftSection>
+          <div className={container}>
+            <Image
+              className={profilePic}
+              priority={true}
+              placeholder='blue'
+              src='/images/me.jpeg'
+              quality={100}
+              width={250}
+              height={250}
+              alt='Picture of the Amjad Yahia'
+            />
+          </div>
 
-        <SectionTitle main center>
-          Welcome To <br />
-          My Personal Portfolio
-        </SectionTitle>
+          <SectionTitle main center>
+            Welcome To <br />
+            My Personal Portfolio
+          </SectionTitle>
 
-        <SectionText>
-          My name is Amjad Yahia and I'm a Web Developer
-        </SectionText>
-        <Button onClick={props.handleClick}>Hire Me</Button>
-      </LeftSection>
-    </Section>
-  </>
-);
+          <SectionText>
+            My name is Amjad Yahia and I'm a Web Developer
+          </SectionText>
+
+          <form method='get' action='/cv/AmjadYahia.pdf'>
+            <Button>View CV</Button>
+          </form>
+        </LeftSection>
+      </Section>
+    </>
+  );
+};
 
 export default Hero;
